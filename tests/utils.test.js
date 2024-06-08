0629ab20c07cfed8e3b5b60879e3a9e70a5895c7
byt3h3ad <adhirajd007@gmail.com>
2024-06-08 15:31:57 +0530

    feat: add tests

---
+++ tests/utils.test.js
@@ -0,0 +1,24 @@
+import { expect, test } from "@jest/globals";
+import {
+  isValidProjectName,
+  toValidProjectName,
+} from "../dist/src/utils/index.js";
+
+test("Check for valid project name:", () => {
+  expect(isValidProjectName("projectname")).toBe(true);
+  expect(isValidProjectName("project-name")).toBe(true);
+  expect(isValidProjectName("project_name")).toBe(true);
+  expect(isValidProjectName("projectName")).toBe(false);
+  expect(isValidProjectName("project name")).toBe(false);
+  expect(isValidProjectName("@scope/ProjectName")).toBe(false);
+  expect(isValidProjectName("scope:projectname")).toBe(false);
+});
+
+test("Convert project name to valid project name:", () => {
+  expect(toValidProjectName("project name")).toBe("project-name");
+  expect(toValidProjectName("projectName")).toBe("projectname");
+  expect(toValidProjectName("project.name")).toBe("project-name");
+  expect(toValidProjectName("project_name")).toBe("project-name");
+  expect(toValidProjectName("@scope/ProjectName")).toBe("-scope-projectname");
+  expect(toValidProjectName("scope:projectname")).toBe("scope-projectname");
+});
